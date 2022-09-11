bdd5c042b707be91790eff142d1ddd07f9f2a538
nigelmarongere <59567927+nigelmarongere@users.noreply.github.com>
2022-09-11 09:08:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-dom-rf1hw-mg8hlw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-dom-rf1hw-mg8hlw)
---
+++ main.js
@@ -0,0 +1,19 @@
+const frodoQuote = document.querySelector('#frodo');
+frodoQuote.innerText = frodoQuote.innerText.replace('revenge', 'pizza');
+
+const samQuote = document.querySelector('#samwise');
+samQuote.innerText = samQuote.innerText.replace('pie', 'stew');
+
+const quotes = document.getElementsByTagName('p');
+for (let quote of quotes){
+  let text = quote.innerText;
+  text = '\"' + text + '\"';
+  quote.innerText = text;
+}
+
+const newQuote = document.createElement('p');
+newQuote.innerHTML = '\"I can ride and wield blade, and I do not fear either pain or death.\"';
+newQuote.setAttribute('id', 'eowyn');
+const article = document.getElementById('quotes');
+article.appendChild(newQuote);
+
